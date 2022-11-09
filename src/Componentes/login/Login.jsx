6cf6ddda2b1a04e18b4d3129f9c2f3059c5119d9
Thedestructive <perguzerick@gmail.com>
2022-11-09 09:24:57 -0600

    Login actualizado
    Ya funciona (segun) el login

--- src/Componentes/login/Login.jsx
+++ src/Componentes/login/Login.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './Login.css'
+import { Link } from 'react-router-dom';
 const Login = () => {
+    function goToHome(){
+
+    }
     return (
         <div className='contenedor-login'>
             <div className='contenedor-img'></div>
             <input type='email' placeholder='hello@email.com' className='input-login' />
             <input type='password' placeholder='********' className='input-login'/>
-            <button className='btn-login'>Log In</button>
+            <button onClick={()=>{goToHome()}} className='btn-login'><Link to={"/Inicio"} className='item-nav'>Log In</Link></button>
+            
         </div>
     )
 }
